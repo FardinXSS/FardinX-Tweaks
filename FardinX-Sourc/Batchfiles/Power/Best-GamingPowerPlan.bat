3af37617d74be14dc6e35fdc000e46ca566e2af2
FardinXSS <163695138+FardinXSS@users.noreply.github.com>
2024-04-10 11:42:59 -0700

    Update Best-GamingPowerPlan.bat

--- FardinX-Sourc/Batchfiles/Power/Best-GamingPowerPlan.bat
+++ FardinX-Sourc/Batchfiles/Power/Best-GamingPowerPlan.bat
@@ -4,5 +4,5 @@ powercfg /changename c9d62faa-c1d7-4ba2-b5ad-2582b27c5952 "FardinX" "This the be
 powercfg /setactive c9d62faa-c1d7-4ba2-b5ad-2582b27c5952
 
 powercfg /hibernate off
-
+PowerCfg /SETACVALUEINDEX SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 000
 powercfg -setacvalueindex scheme_current sub_processor 5d76a2ca-e8c0-402f-a133-2158492d58ad 1
