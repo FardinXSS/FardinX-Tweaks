b6c425652b5cd7fb73ce3103458512a947f8e80d
FardinXSS <163695138+FardinXSS@users.noreply.github.com>
2024-04-10 09:21:13 -0700

    Update Best-GamingPowerPlan.bat

--- FardinX-Sourc/Batchfiles/Best-GamingPowerPlan.bat
+++ FardinX-Sourc/Batchfiles/Best-GamingPowerPlan.bat
@@ -3,5 +3,6 @@ powercfg /duplicatescheme scheme_current c9d62faa-c1d7-4ba2-b5ad-2582b27c5952
 powercfg /changename c9d62faa-c1d7-4ba2-b5ad-2582b27c5952 "FardinX" "This the best powerplan you ever need"
 powercfg /setactive c9d62faa-c1d7-4ba2-b5ad-2582b27c5952
 
-@echo Disable Hibernate
 powercfg /hibernate off
+
+powercfg -setacvalueindex scheme_current sub_processor 5d76a2ca-e8c0-402f-a133-2158492d58ad 100
