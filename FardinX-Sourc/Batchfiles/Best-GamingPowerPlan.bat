638fd9c78e3cb34fec23730bb3ab2f93a9a9374c
FardinXSS <163695138+FardinXSS@users.noreply.github.com>
2024-04-10 08:25:45 -0700

    Update Best-GamingPowerPlan.bat

--- FardinX-Sourc/Batchfiles/Best-GamingPowerPlan.bat
+++ FardinX-Sourc/Batchfiles/Best-GamingPowerPlan.bat
@@ -1,7 +1,7 @@
 @echo Create, rename and activate new power plan
-powercfg /duplicatescheme scheme_current c300f18e-cd99-4b8c-9690-afe10ec33e05
-powercfg /changename c300f18e-cd99-4b8c-9690-afe10ec33e05 "TornadoPower" "TornadoPower"
-powercfg /setactive c300f18e-cd99-4b8c-9690-afe10ec33e05
+powercfg /duplicatescheme scheme_current c9d62faa-c1d7-4ba2-b5ad-2582b27c5952
+powercfg /changename c9d62faa-c1d7-4ba2-b5ad-2582b27c5952 "TornadoPower" "TornadoPower"
+powercfg /setactive c9d62faa-c1d7-4ba2-b5ad-2582b27c5952
 
 @echo Disable Hibernate
 powercfg /hibernate off
